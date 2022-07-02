5b6fb280ab93cd9a173142c12b2b8dd95ee8b45d
btcnoder <btcnoder@gmail.com>
2022-07-02 19:09:37 +0000

    feat: tailwind automatic classname sorting

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ function App() {
     <>
       <div className="container mx-auto px-4 py-10">
         <div className="mx-8 grid grid-cols-1 gap-4 md:grid-cols-2">
+          <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+            class 11
+          </button>
+          <div className="p-4 pt-2"></div>
           <div>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
           </div>
