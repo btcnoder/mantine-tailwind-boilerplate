8a9c498982846f59b64033db36e5ecf505e024a7
btcnoder <btcnoder@gmail.com>
2022-07-02 19:24:56 +0000

    docs: update README

---
+++ README.md
@@ -0,0 +1,7 @@
+### mantine-tailwind-boilerplate
+
+The code repository is composed of mantine + tailwindcss + vite, which is a set of boilerplate code. At the same time added a plugin for automatic sorting of class names。
+
+### LICENSE
+
+MIT
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
-import { Button, Slider } from '@mantine/core'
-import { useState } from 'react'
+import { Button } from '@mantine/core'
 
 function App() {
-  const [value, setValue] = useState(50)
-  const [endValue, setEndValue] = useState(50)
   return (
     <>
       <div className="container mx-auto px-4 py-10">
         <div className="mx-8 grid grid-cols-1 gap-4 md:grid-cols-2">
           <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-            class 11
+            class name sort
           </button>
           <div className="p-4 pt-2"></div>
           <div>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
           </div>
-
           <Button>Button</Button>
-          <div>
-            <Slider
-              value={value}
-              onChange={setValue}
-              onChangeEnd={setEndValue}
-              marks={[
-                { value: 0, label: 'xs' },
-                { value: 25, label: 'sm' },
-                { value: 50, label: 'md' },
-                { value: 75, label: 'lg' },
-                { value: 100, label: 'xl' }
-              ]}
-            />
-            <div className="pt-8 text-base text-slate-800">
-              onChange value: {value}
-            </div>
-            <div className="text-base text-slate-800">
-              onChangeEnd value:{endValue}
-            </div>
-          </div>
         </div>
       </div>
     </>
