c4e77108b68bc470bad37fbc156fe90e6f2bdfbb
btcnoder <btcnoder@gmail.com>
2022-07-03 09:39:40 +0000

    fix: modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@mantine/core'
+import { MantineProvider, Text } from '@mantine/core'
 
 function App() {
   return (
-    <>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
       <div className="container mx-auto px-4 py-10">
+        <Text>Welcome to Mantine!</Text>
         <div className="mx-8 grid grid-cols-1 gap-4 md:grid-cols-2">
           <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
             class name sort
@@ -15,8 +17,7 @@ function App() {
           <Button>Button</Button>
         </div>
       </div>
-    </>
+    </MantineProvider>
   )
 }
-
 export default App
